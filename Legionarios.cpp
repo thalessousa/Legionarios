0819d29b65282dd2dddc41d43d1c7e221c2daa3f
thales <thales@10.15.7.164>
2014-10-03 11:40:38 -0300

    Commit 1

---
+++ Legionarios.cpp
@@ -0,0 +1,16 @@
+#include "Legionarios.h"
+
+namespace legionarios
+{
+
+Legionarios::Legionarios()
+{
+}
+
+Legionarios::~Legionarios()
+{
+}
+
+
+}
+
---
+++ Legionarios.h
@@ -0,0 +1,22 @@
+#ifndef LEGIONARIOS_H
+#define LEGIONARIOS_H
+
+namespace legionarios
+{
+
+class Legionarios
+{
+public:
+	Legionarios();
+    
+private:
+	static const int dano;
+	static const int velocidade;
+	              
+	 
+
+};
+
+}
+
+#endif // LEGIONARIOS_H
